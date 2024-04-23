82a8d7db52065d9373c2ef729960e5b4f3f7badf
alasaidi <alasaidi55gmail.com>
2024-04-23 19:33:46 +0200

    deep-flat challenge done issues #3

---
+++ src/deep-flat/ala-solution/deepFlat.spec.js
@@ -0,0 +1,21 @@
+import { deepFlat } from './deepFlat.js';
+
+describe('Converts a nested array into a single array with no nesting', () => {
+  it('an empty array should return an empty array', () => {
+    expect(deepFlat([])).toEqual([]);
+  });
+
+  it('[1, [2, [3, [4]], 5]]-> [1, 2, 3, 4, 5]', () => {
+    expect(deepFlat([1, [2, [3, [4]], 5]])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('["a", ["b", ["c"], ["g"]], "h"]]-> ["a", "b", "c", "g", "h"]', () => {
+    expect(deepFlat(['a', ['b', ['c'], ['g']], 'h'])).toEqual([
+      'a',
+      'b',
+      'c',
+      'g',
+      'h',
+    ]);
+  });
+});
